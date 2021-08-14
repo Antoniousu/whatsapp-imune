82a4cd3b014ed6964395ea08dc10f9226f7565c8
Antoniousu <87615395+Antoniousu@users.noreply.github.com>
2021-08-14 09:01:30 -0300

    Add files via upload

---
+++ paginas/Script2.js
@@ -0,0 +1,4 @@
+function confirmBox() {
+if (confirm("Você Deseja Ir Para Central De Bugs E Central De Sugestões No Whatsapp?")) {
+location.href="https://api.whatsapp.com/send?phone=552298470588&text=Olá%20Eu%20Vim%20Pelo%20Seu%3FSite%20Meu%20Nome%20É%20:";}
+}
